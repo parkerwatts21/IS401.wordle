be6f59f1142a4307dd03864f7115ac5bb7e5a50b
parkerwatts21 <parkerwatts21@gmail.com>
2024-01-13 18:41:18 -0700

    Milestone #1

--- IS401/Wordle.py
+++ IS401/Wordle.py
@@ -1,11 +1,10 @@
-# Parker Watts, Aiki Takaku, Donna Kim, Mac Hartsell
-# Description : Write a python program that simulates the game wordle
-
 # File: Wordle.py
 
 """
-This module is the starter file for the Wordle assignment.
-BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
+Parker Watts, Aiki Takaku, Donna Kim, Mac Hartsell
+Description : Write a python program that simulates the game wordle
+
+I have completed Milestone 1 so far. I added comments to see where I added my code
 """
 
 import random
@@ -21,16 +20,21 @@ def wordle(sWordOfTheDay):
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
+    # Putting the the word of the day using the function Set_Square_Letter and looping through each letter - (Parker) MILESTONE #1
     for iCount in range (0, 5, 1) :
         sLetter = sWordOfTheDay[iCount]
-        gw.get_square_letter(1, iCount, sLetter)
+        gw.set_square_letter(N_ROWS - 5, iCount, sLetter)
 
-# Startup code
+
+# Selecting a random word from the dicionary and setting variables - (Parker) MILESTONE #1 
 iNum = random.randrange(0, len(FIVE_LETTER_WORDS) + 1)
 sWordOfTheDay = FIVE_LETTER_WORDS[iNum]
+
+# Startup code
 
 if __name__ == "__main__":
     wordle(sWordOfTheDay)
 
+# Testing to see what the random word is by printing - (Parker) MILESTONE #1
 print(iNum)
 print(sWordOfTheDay)
